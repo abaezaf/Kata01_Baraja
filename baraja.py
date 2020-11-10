8632a352ee702cf6fa349c548de6d7d76340a5c9
abaezaf <antonio.baezafdez@gmail.com>
2020-11-11 00:25:14 +0100

    segunda versión de baraja

--- baraja.py
+++ baraja.py
@@ -1,9 +1,8 @@
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
-baraja = []
-
 def crearBaraja():
+    baraja = []
     for palo in palos:
         for numero in numeros:
             baraja.append(numero+palo)
