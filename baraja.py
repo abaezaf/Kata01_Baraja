e45438ac0eaa03a32ea2b75e470f6d74822a466e
abaezaf <antonio.baezafdez@gmail.com>
2020-11-11 22:32:02 +0100

    Función Barajar OK

--- baraja.py
+++ baraja.py
@@ -1,3 +1,5 @@
+import random
+
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
@@ -7,3 +9,21 @@ def crearBaraja():
         for numero in numeros:
             baraja.append(numero+palo)
     return baraja
+
+def intercambio(a, b):
+    aux = a
+    a = b
+    b = aux
+    return a, b
+
+def barajar(lista_naipes):
+    for i in range(len(lista_naipes)):
+        nueva_posicion = random.randrange(len(lista_naipes))
+        
+        #Intercambio con vasos vacíos
+        
+        aux = lista_naipes[i]
+        lista_naipes[i] = lista_naipes[nueva_posicion]
+        lista_naipes[nueva_posicion] = aux
+    return lista_naipes
+
