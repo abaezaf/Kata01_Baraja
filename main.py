21cc8d836662dbb5b2b7d3b6c8ac2c2be9f3918c
abaezaf <antonio.baezafdez@gmail.com>
2020-11-12 22:09:21 +0100

    Kata final

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import barajaC
 
-palos = ["treboles", "corazones", "picas", "diamantes"]
-numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+palos = ["o", "c", "b", "e"]
+numeros = ["A", "2", "3", "4", "5", "6", "7", "s", "c", "r"]
 
 
 mibaraja = barajaC.Baraja(palos, numeros)
@@ -9,3 +9,5 @@ mibaraja = barajaC.Baraja(palos, numeros)
 print(mibaraja.mazo)
 mibaraja.barajar()
 print(mibaraja.mazo)
+print(mibaraja.repartir(5,5))
+
